6307fa026b4c0aeb6c7609e2b55033a2924190e0
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-15 11:12:08 +0900

    新規登録のテストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                  { 'test' }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa000' }
+    password_confirmation { 'aaa000' }
+  end
+end
