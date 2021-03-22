cb023594076f27feaa3e50f1f1905dcb90b13f48
yumoto34492 <78357097+yumoto34492@users.noreply.github.com>
2021-03-22 12:11:37 +0900

    更新日順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @name = current_user.name
-    @targets = current_user.targets.order("created_at DESC")
+    @targets = current_user.targets.order("updated_at DESC")
   end
 end
